e32d669984b9f88187a17f170b5ea77ef2eca394
VipulbhaiMalakiya <vipul.malakiya@teamcomputers.com>
2024-11-27 16:29:16 +0530

    update

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -11,6 +11,13 @@ import { AssigntomeComponent } from './pages/assigntome/assigntome.component';
 import { DashboardTicketFilter } from 'src/app/_helpers/dashboard ticket-filter';
 import { FormsModule } from '@angular/forms';
 import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
     declarations: [
@@ -27,7 +34,15 @@ import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
         SharedModule,
         GoogleChartsModule,
         FormsModule,
-        CanvasJSAngularChartsModule
+        CanvasJSAngularChartsModule,
+        MatDatepickerModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatNativeDateModule,
+        MatButtonModule,
+        MatSelectModule
+
+
     ]
 })
 export class DashboardModule { }
