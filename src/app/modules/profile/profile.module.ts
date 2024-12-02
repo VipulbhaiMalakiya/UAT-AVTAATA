df7572130cce1550cbd48400ed35a06f3f5088f5
VipulbhaiMalakiya <vipul.malakiya@teamcomputers.com>
2024-12-02 12:00:53 +0530

    update

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -4,17 +4,26 @@ import { ProfileRoutingModule } from './profile-routing.module';
 import { MyProfileComponent } from './my-profile/my-profile.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
-  declarations: [
-    MyProfileComponent
-  ],
-  imports: [
-    CommonModule,
-    ProfileRoutingModule,
-    ReactiveFormsModule,
-    SharedModule
-  ]
+    declarations: [
+        MyProfileComponent
+    ],
+    imports: [
+        CommonModule,
+        ProfileRoutingModule,
+        ReactiveFormsModule,
+        SharedModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+        MatCheckboxModule,
+    ]
 })
 export class ProfileModule { }
