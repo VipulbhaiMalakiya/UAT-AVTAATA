6c3995b6a89b6ffb9fe2935da231c617514e06ee
VipulbhaiMalakiya <vipul.malakiya@teamcomputers.com>
2024-12-06 15:30:48 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,37 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AuthenticationService } from './_services/authentication.service';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'avataara';
-  constructor(private authenticationService: AuthenticationService,
-    private router: Router,) {
+export class AppComponent implements OnInit {
+    title = 'avataara';
+    constructor(private authenticationService: AuthenticationService,
+        private router: Router, private toastr: ToastrService,) {
 
-  }
+    }
+
+
+    ngOnInit() {
+        // Initial online/offline check
+        if (navigator.onLine) {
+            // this.toastr.success('You are online!', 'Status');
+        } else {
+            this.toastr.error('You are offline!', 'Status');
+        }
+
+        // Listen to the 'online' event
+        window.addEventListener('online', () => {
+            this.toastr.success('You are back online!', 'Status');
+        });
+
+        // Listen to the 'offline' event
+        window.addEventListener('offline', () => {
+            this.toastr.error('You have lost your connection!', 'Status');
+        });
+    }
+
+
 }
