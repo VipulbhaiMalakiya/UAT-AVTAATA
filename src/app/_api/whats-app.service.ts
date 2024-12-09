8d428e8360242118bba06c3177bb7317a988eed0
VipulbhaiMalakiya <vipul.malakiya@teamcomputers.com>
2024-12-09 09:32:37 +0530

    update

--- src/app/_api/whats-app.service.ts
+++ src/app/_api/whats-app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HeadersService } from '../_services/headers.service';
 import { WebSocketSubject } from 'rxjs/webSocket';
-import { map } from 'rxjs/operators';
+import { catchError, map, shareReplay, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -13,6 +13,10 @@ export class WhatsAppService {
 
     private socket$!: WebSocketSubject<any>;
     private baseUrl: string = environment.apiUrl;
+
+    private chatCache = new Map<string, Observable<any>>();
+    private ongoingRequests = new Map<string, boolean>();
+
     constructor(private http: HttpClient,
         private header: HeadersService) {
     }
@@ -29,11 +33,50 @@ export class WhatsAppService {
     //     return this.http.get(this.baseUrl + `/chatlist/history/number/${request}`, httpOptions);
     // }
 
-    chatHistorynew(contact: string, currentPage: number, pageSize: number) {
+    // chatHistorynew(contact: string, currentPage: number, pageSize: number) {
+    //     let headers = this.header.getJWTHeaders();
+    //     const httpOptions = { headers: headers };
+    //     const url = `${this.baseUrl}/chatlist/historypagination/number/${contact}?page=${currentPage}&pageSize=${pageSize}`;
+    //     return this.http.get(url, httpOptions);
+    // }
+
+    chatHistorynew(contact: string, page: number, pageSize: number): Observable<any> {
+        const cacheKey = `${contact}_${page}`;
+
+
         let headers = this.header.getJWTHeaders();
         const httpOptions = { headers: headers };
-        const url = `${this.baseUrl}/chatlist/historypagination/number/${contact}?page=${currentPage}&pageSize=${pageSize}`;
-        return this.http.get(url, httpOptions);
+        const url = `${this.baseUrl}/chatlist/historypagination/number/${contact}?page=${page}&pageSize=${pageSize}`;
+
+        // Check if the request is already ongoing
+        if (this.chatCache.has(cacheKey)) {
+            return this.chatCache.get(cacheKey)!; // Return the cached observable
+        }
+
+        // Fetch data and cache it
+        const request = this.http
+            .get<any>(url, httpOptions)
+            .pipe(
+                tap(() => {
+                    // Mark request as complete
+                    this.ongoingRequests.delete(cacheKey);
+                }),
+                shareReplay(1), // Share the request among multiple subscribers
+                catchError((error) => {
+                    this.chatCache.delete(cacheKey); // Remove failed request from cache
+                    this.ongoingRequests.delete(cacheKey);
+                    throw error;
+                })
+            );
+
+        this.chatCache.set(cacheKey, request);
+        return request;
+    }
+
+    clearCache(contact: string): void {
+        Array.from(this.chatCache.keys())
+            .filter((key) => key.startsWith(contact))
+            .forEach((key) => this.chatCache.delete(key));
     }
 
 
