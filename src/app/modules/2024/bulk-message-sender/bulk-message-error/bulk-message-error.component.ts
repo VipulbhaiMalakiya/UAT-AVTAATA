3d36f271517dde801df8d0827b88ca2b59ad566b
Vipul Malakiya <vipul.malakiya@teamcomputers.com>
2024-12-18 14:47:25 +0530

    update

--- src/app/modules/2024/bulk-message-sender/bulk-message-error/bulk-message-error.component.ts
+++ src/app/modules/2024/bulk-message-sender/bulk-message-error/bulk-message-error.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-bulk-message-error',
-  templateUrl: './bulk-message-error.component.html',
-  styleUrls: ['./bulk-message-error.component.css']
+    selector: 'app-bulk-message-error',
+    templateUrl: './bulk-message-error.component.html',
+    styleUrls: ['./bulk-message-error.component.css']
 })
-export class BulkMessageErrorComponent {
+export class BulkMessageErrorComponent implements OnInit {
+    private _customersMaster: any | undefined;
+    isProceess: boolean = false;
+    data: any;
 
+
+
+    set customersMaster(value: any) {
+        this._customersMaster = value;
+
+    }
+
+    constructor(
+        private activeModal: NgbActiveModal,
+    ) {
+
+    }
+
+    ngOnInit(): void {
+        this.data = this._customersMaster;
+    }
+
+    onCancel() {
+        this.isProceess = false;
+        this.activeModal.dismiss();
+    }
 }
